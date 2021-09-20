c16bb5cf4037e2623343a36d0667233437c0cf59
Ichsanul Alifwan <ichsanulalifwan@gmail.com>
2021-09-21 03:34:56 +0800

    test TTS and STT for usecase

--- app/src/main/java/com/app/tunanetradaily/GiziSeimbangFragment.kt
+++ app/src/main/java/com/app/tunanetradaily/GiziSeimbangFragment.kt
@@ -1,31 +1,31 @@
 package com.app.tunanetradaily
 
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.app.tunanetradaily.databinding.FragmentGiziSeimbangBinding
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [GiziSeimbangFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class GiziSeimbangFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private var _binding: FragmentGiziSeimbangBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    private val textToSpeechEngine: TextToSpeech by lazy {
+        // Pass in context and the listener.
+        TextToSpeech(
+            context
+        ) { status ->
+            // set our locale only if init was success.
+            if (status == TextToSpeech.SUCCESS) {
+                textToSpeechEngine.language = Locale("id", "ID")
+            }
         }
     }
 
@@ -34,26 +34,21 @@ class GiziSeimbangFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gizi_seimbang, container, false)
+        _binding = FragmentGiziSeimbangBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GiziSeimbangFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GiziSeimbangFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (activity != null)  {
+
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/app/tunanetradaily/MainActivity.kt
+++ app/src/main/java/com/app/tunanetradaily/MainActivity.kt
@@ -8,24 +8,28 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import android.speech.RecognizerIntent
 import android.speech.tts.TextToSpeech
+import android.speech.tts.UtteranceProgressListener
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.app.tunanetradaily.databinding.ActivityMainBinding
 import java.util.*
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var textToSpeechEngine: TextToSpeech? = null
 
-    private val textToSpeechEngine: TextToSpeech by lazy {
+    private val textToSpeechEngine2: TextToSpeech by lazy {
         // Pass in context and the listener.
         TextToSpeech(
             this
         ) { status ->
             // set our locale only if init was success.
             if (status == TextToSpeech.SUCCESS) {
-                textToSpeechEngine.language = Locale("id", "ID")
+                textToSpeechEngine2.language = Locale("id", "ID")
             }
         }
     }
@@ -47,51 +51,111 @@ class MainActivity : AppCompatActivity() {
             checkPermission()
         }
 
-        binding.btnSpeak.setOnClickListener {
-            // Get the Intent action
-            val sttIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
-            // Language model defines the purpose, there are special models for other use cases, like search.
-            sttIntent.putExtra(
-                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
-            )
-            // Adding an extra language, you can use any language from the Locale class.
-            sttIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale("id", "ID"))
-            // Text that shows up on the Speech input prompt.
-            sttIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak now!")
-            try {
-                // Start the intent for a result, and pass in our request code.
-                startActivityForResult(sttIntent, REQUEST_CODE_STT)
-            } catch (e: ActivityNotFoundException) {
-                // Handling error when the service is not available.
-                e.printStackTrace()
-                Toast.makeText(
-                    this,
-                    "Your device does not support SpeechRecognizer.",
-                    Toast.LENGTH_LONG
-                ).show()
+        textToSpeechEngine = TextToSpeech(this) { arg0 ->
+            if (arg0 == TextToSpeech.SUCCESS) {
+                // Set language
+                textToSpeechEngine?.language = Locale("id", "ID")
+
+                // start speech welcome message
+                welcomeTTS()
             }
         }
 
+        litkesTTS()
+
+        binding.btnSpeak.setOnClickListener {
+            Toast.makeText(this, "btnSpeak Clicked", Toast.LENGTH_LONG).show()
+        }
+
         binding.btnListen.setOnClickListener {
-            // Get the text to be converted to speech from our EditText.
+            litkesTTS()
+            /*// Get the text to be converted to speech from our EditText.
             val text = binding.tvSpeak.text.toString()
             // Check if user hasn't input any text.
             if (text.isNotEmpty()) {
                 // Lollipop and above requires an additional ID to be passed.
                 // Call Lollipop+ function
-                textToSpeechEngine.speak(text, TextToSpeech.QUEUE_FLUSH, null, "tts1")
+                textToSpeechEngine.speak(text, TextToSpeech.QUEUE_FLUSH, null, "tts")
             } else {
                 Toast.makeText(this, "Text cannot be empty", Toast.LENGTH_LONG).show()
-            }
+            }*/
         }
+    }
+
+    private fun welcomeTTS() {
+        // Get the text from local string resource
+        val text = getString(R.string.welcome_message)
+
+        // Lollipop and above requires an additional ID to be passed.
+        // Call Lollipop+ function
+        textToSpeechEngine?.speak(text, TextToSpeech.QUEUE_FLUSH, null, "tts1")
+
+        textToSpeechEngine?.setOnUtteranceProgressListener(object : UtteranceProgressListener() {
+            override fun onStart(utteranceId: String?) {
+                Log.i("TextToSpeech", "On Start")
+            }
+
+            // Get Input speech after TTS Done
+            override fun onDone(utteranceId: String?) {
+                Log.i("TextToSpeech", "On Done")
+
+                // Get the Intent action
+                val sttIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+                // Language model defines the purpose, there are special models for other use cases, like search.
+                sttIntent.putExtra(
+                    RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+                )
+                // Adding an extra language, you can use any language from the Locale class.
+                sttIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale("id", "ID"))
+                // Text that shows up on the Speech input prompt.
+                sttIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Bicara Sekarang!")
+                try {
+                    // Start the intent for a result, and pass in our request code.
+                    startActivityForResult(sttIntent, REQUEST_CODE_STT1)
+                } catch (e: ActivityNotFoundException) {
+                    // Handling error when the service is not available.
+                    e.printStackTrace()
+                    Toast.makeText(
+                        applicationContext,
+                        "Your device does not support SpeechRecognizer.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+
+            override fun onError(utteranceId: String?) {
+                Log.i("TextToSpeech", "On Error")
+            }
+        })
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         when (requestCode) {
             // Handle the result for our request code.
-            REQUEST_CODE_STT -> {
+            REQUEST_CODE_STT1 -> {
+                // Safety checks to ensure data is available.
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    // Retrieve the result array.
+                    val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    // Ensure result array is not null or empty to avoid errors.
+                    if (!result.isNullOrEmpty()) {
+                        // Recognized text is in the first position.
+                        val recognizedText = result[0]
+                        val choice = "satu"
+                        // Do what you want with the recognized text.
+                        binding.tvSpeak.text = recognizedText
+
+                        if (recognizedText.equals(choice, true) || recognizedText == "1")
+                            litkesTTS()
+                        else
+                            Toast.makeText(this, "Gagal", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+            REQUEST_CODE_STT2 -> {
                 // Safety checks to ensure data is available.
                 if (resultCode == Activity.RESULT_OK && data != null) {
                     // Retrieve the result array.
@@ -102,10 +166,61 @@ class MainActivity : AppCompatActivity() {
                         val recognizedText = result[0]
                         // Do what you want with the recognized text.
                         binding.tvSpeak.text = recognizedText
+
+                        if (recognizedText.equals("nol", true) || recognizedText == "0") {
+                            finish()
+                            exitProcess(0)
+                        } else {
+                            Toast.makeText(this, "Gagal", Toast.LENGTH_SHORT).show()
+                        }
                     }
                 }
             }
         }
+    }
+
+    private fun litkesTTS() {
+        val textLitkes = getString(R.string.litkes_menu)
+        textToSpeechEngine2.speak(textLitkes, TextToSpeech.QUEUE_FLUSH, null, "tts")
+
+        textToSpeechEngine2.setOnUtteranceProgressListener(object : UtteranceProgressListener() {
+            override fun onStart(utteranceId: String?) {
+                Log.i("TextToSpeech", "On Start")
+            }
+
+            // Get Input speech after TTS Done
+            override fun onDone(utteranceId: String?) {
+                Log.i("TextToSpeech", "On Done")
+
+                // Get the Intent action
+                val sttIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+                // Language model defines the purpose, there are special models for other use cases, like search.
+                sttIntent.putExtra(
+                    RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+                )
+                // Adding an extra language, you can use any language from the Locale class.
+                sttIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale("id", "ID"))
+                // Text that shows up on the Speech input prompt.
+                sttIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Bicara Sekarang!")
+                try {
+                    // Start the intent for a result, and pass in our request code.
+                    startActivityForResult(sttIntent, REQUEST_CODE_STT2)
+                } catch (e: ActivityNotFoundException) {
+                    // Handling error when the service is not available.
+                    e.printStackTrace()
+                    Toast.makeText(
+                        applicationContext,
+                        "Your device does not support SpeechRecognizer.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+
+            override fun onError(utteranceId: String?) {
+                Log.i("TextToSpeech", "On Error")
+            }
+        })
     }
 
     private fun checkPermission() {
@@ -113,16 +228,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onPause() {
-        textToSpeechEngine.stop()
+        textToSpeechEngine?.stop()
         super.onPause()
     }
 
     override fun onDestroy() {
-        textToSpeechEngine.shutdown()
+        textToSpeechEngine?.shutdown()
         super.onDestroy()
     }
 
     companion object {
-        private const val REQUEST_CODE_STT = 1
+        private const val REQUEST_CODE_STT1 = 1
+        private const val REQUEST_CODE_STT2 = 2
     }
 }
