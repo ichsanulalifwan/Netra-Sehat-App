1cc399459e9ced9ae59056f0ae6b533562b99ca2
Ichsanul Alifwan <ichsanulalifwan@gmail.com>
2021-10-24 15:29:20 +0800

    add Splash Screen

---
+++ app/src/main/java/com/app/netrasehat/SplashScreenActivity.kt
@@ -0,0 +1,37 @@
+package com.app.netrasehat
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatDelegate
+import com.app.netrasehat.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val main = Intent(this, MainActivity::class.java)
+            startActivity(main)
+            finish()
+        }, TIME)
+    }
+
+    companion object {
+        private const val TIME: Long = 3000
+    }
+}
